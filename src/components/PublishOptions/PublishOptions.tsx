87f8201d466a05f421aaedbad53644c1454b6006
Viet <viet@r-digital.tech>
2023-06-20 17:46:08 +0700

    Remove debug

--- src/components/PublishOptions/PublishOptions.tsx
+++ src/components/PublishOptions/PublishOptions.tsx
@@ -90,10 +90,6 @@ const PublishOptionsDetail = observer(
                         PIM_PRODUCT_DETAIL_FIELD_KEY.PUBLISHED,
                         data.value
                       );
-                      console.log(
-                        'formPropsData[PIM_PRODUCT_DETAIL_FIELD_KEY.PUBLISHED]',
-                        formPropsData[PIM_PRODUCT_DETAIL_FIELD_KEY.PUBLISHED]
-                      );
                     },
                   }}
                 />
