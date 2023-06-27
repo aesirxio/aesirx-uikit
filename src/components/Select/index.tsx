36e7461a9fca76d87fa2056028a49d15461cd462
Viet <viet@r-digital.tech>
2023-06-27 12:00:59 +0700

    Select Group

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -3,7 +3,7 @@
  * @license     GNU General Public License version 3, see LICENSE.
  */
 
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 import Select, { components } from 'react-select';
 import AsyncSelect from 'react-select/async';
@@ -52,6 +52,32 @@ class AesirXSelect extends React.Component<any, any> {
         </ValueContainer>
       );
     };
+
+    const groupStyles = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    };
+
+    const groupBadgeStyles: CSSProperties = {
+      backgroundColor: '#EBECF0',
+      borderRadius: '2em',
+      color: '#172B4D',
+      display: 'inline-block',
+      fontSize: 12,
+      fontWeight: 'normal',
+      lineHeight: '1',
+      minWidth: 1,
+      padding: '0.16666666666667em 0.5em',
+      textAlign: 'center',
+    };
+
+    const formatGroupLabel = (data: any) => (
+      <div style={groupStyles}>
+        <span>{data.label}</span>
+        <span style={groupBadgeStyles}>{data.options.length}</span>
+      </div>
+    );
     return (
       <Select
         {...this.props}
@@ -63,6 +89,7 @@ class AesirXSelect extends React.Component<any, any> {
         isClearable={isClearable}
         placeholder={placeholder ?? t('txt_select')}
         styles={styles}
+        formatGroupLabel={formatGroupLabel}
       />
     );
   }
