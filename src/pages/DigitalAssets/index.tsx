b9c1e5d751ccc42a9c12b0c2bd9a65b90b952595
toanredweb <toan@r-digital.tech>
2023-08-19 16:11:54 +0700

    Update UI

--- src/pages/DigitalAssets/index.tsx
+++ src/pages/DigitalAssets/index.tsx
@@ -8,7 +8,7 @@ import { AesirXDam } from 'aesirx-dam-app';
 
 function DigitalAssetsPage() {
   return (
-    <div className="py-4 px-3 h-100 bg-body ">
+    <div className="py-4 px-3 h-100">
       <AesirXDam />
     </div>
   );
