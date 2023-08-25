8af958f357228a64f13f8e421e962ddbf86e89f9
Viet <viet@r-digital.tech>
2023-08-25 16:31:34 +0700

    Update input disabled bg

--- src/components/Select/customStyles.ts
+++ src/components/Select/customStyles.ts
@@ -25,7 +25,7 @@ const customStyles = (
           // borderRight: '1px solid var(--aesirxui-border-color)',
         },
         // borderRight: '1px solid var(--aesirxui-border-color)',
-        backgroundColor: isDisabled ? 'var(--aesirxui-gray-dark)' : 'var(--aesirxui-white)',
+        backgroundColor: isDisabled ? 'var(--aesirxui-input-disabled-bg)' : 'var(--aesirxui-white)',
         cursor: 'pointer',
         width: 'auto',
         minWidth: minWidth,
