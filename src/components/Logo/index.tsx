895c25aac991f5c663c5d37f93f46a8ec92d5118
toanredweb <toan@r-digital.tech>
2023-09-06 17:18:34 +0700

    update

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,12 +6,20 @@
 import React from 'react';
 import { Image } from '../Image';
 import logoWhite from '../../assets/images/logo/logo-company.svg';
+import logoWhiteMini from '../../assets/images/logo/logo-white-mini.svg';
 
-const Logo = ({ logo }: any) => {
+const Logo = ({ isMini, logo }: any) => {
   return (
     <div className="wrapper_header_logo d-xl-flex d-none  w-260 h-80 align-items-center bg-menu">
-      <a href={window.location.href} className="header_logo d-block mt-4">
-        <Image className="logo_white pe-3" src={logo ? logo : logoWhite} alt="AesirX" />
+      <a
+        href={window.location.href}
+        className={`header_logo d-block  ${isMini ? 'mx-auto my-auto' : 'mt-4'}`}
+      >
+        <Image
+          className={`logo_white ${isMini ? 'pe-0' : 'pe-3'}`}
+          src={`${isMini ? logoWhiteMini : logo ? logo : logoWhite}`}
+          alt="AesirX"
+        />
         {/* <p className="fs-sm py-2 text-white">Digital Marketing Automation</p> */}
       </a>
     </div>
