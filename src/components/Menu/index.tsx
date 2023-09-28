eb889aea7c57e08a11520a9d119930abf0607768
toanredweb <toan@r-digital.tech>
2023-09-28 17:18:27 +0700

    update

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -66,17 +66,18 @@ const Menu = ({ dataMenu, title = '' }: any) => {
                     <>
                       {menuList.link && (
                         <NavLink
-                          exact={true}
-                          to={menuList.link}
-                          className={`d-block px-24 py-16 mx-3 rounded link_menu text-white text-decoration-none`}
-                          activeClassName={`active`}
-                          onClick={() => setIsOpenCollapse(null)}
-                        >
-                          {menuList.icons_fa ? (
-                            <i>
-                              <FontAwesomeIcon icon={menuList.icons_fa} />
-                            </i>
-                          ) : (
+                        exact={true}
+                        to={menuList.link}
+                        className={`d-block px-24 py-16 mx-3 rounded link_menu text-white text-decoration-none`}
+                        activeClassName={`active`}
+                        onClick={() => setIsOpenCollapse(null)}
+                      >
+                        {menuList.icons_fa ? (
+                          <i>
+                            <FontAwesomeIcon icon={menuList.icons_fa} />
+                          </i>
+                        ) : (
+                          menuList.icons_color ? (
                             <span
                               className="icon d-inline-block align-text-bottom"
                               style={{
@@ -85,10 +86,13 @@ const Menu = ({ dataMenu, title = '' }: any) => {
                                 backgroundColor: '#fff',
                               }}
                             ></span>
-                          )}
+                          ) : (
+                            null
+                          )
+                        )}
+                        <span className={`text d-inline-block ${menuList.icons_fa ? 'ms-16' : ''}`}>{t(menuList.text)}</span>
 
-                          <span className="ms-16 text d-inline-block">{t(menuList.text)}</span>
-                        </NavLink>
+                      </NavLink>
                       )}
                     </>
                   ) : (
