f479ca2b2f6b0d72a88769b7f0d37b7bc18fc557
trucduy10 <trucduy.truong@gmail.com>
2023-10-10 17:50:15 +0700

    uxui for thumb

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -13,29 +13,24 @@ const Thumb: React.FC<ThumbType> = ({ data }) => {
         {toJS(data).map((item: any) => {
           let status = '';
           if (item.status == 1) {
-            status = 'In Progress';
+            status = 'Running';
           } else if (item.status == 2) {
-            status = 'Pending';
+            status = 'Schedule';
           } else {
-            status = 'Failed';
+            status = 'Stop';
           }
           const classStatus = status.toLowerCase().replace(/\s/g, '');
           return (
             <div className="col-md-3 mb-4 " key={item.id}>
-              <div className="bg-white rounded-3 p-3 h-100">
-                <p className="rounded-3 overflow-hidden">
-                  <Image
-                    src={item.logo ? item.logo : './assets/images/annotation.png'}
-                    alt={item.name.props.children}
-                  />
-                </p>
+              <div className="bg-white rounded-3 p-3 overflow-y-hidden mh-350px">
+                <p>{item.logo}</p>
                 <p>
-                  <span className={`px-2 rounded-1 d-inline-block project__status-${classStatus}`}>
+                  <span className={`px-2 fs-14 rounded-1 d-inline-block project__status-${classStatus}`}>
                     {status}
                   </span>
                 </p>
-                <h3 className="fs-5 fw-bold">{item.name.props.children}</h3>
-                <p>{item.shortDescription}</p>
+                <h3 className=" text-name fs-5 fw-bold">{item.name.props.children}</h3>
+                <p className="text-description overflow-hidden h-100px">{item.shortDescription}</p>
               </div>
             </div>
           );
