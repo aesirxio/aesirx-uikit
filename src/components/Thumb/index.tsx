5f92b050be644257c6b43cac39ff26a9d598ddd2
trucduy10 <trucduy.truong@gmail.com>
2023-10-11 15:32:59 +0700

    translation

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -1,38 +1,46 @@
 import { toJS } from 'mobx';
 import React from 'react';
 import './index.scss';
-import { withTranslation } from 'react-i18next';
+import { withTranslation, useTranslation } from 'react-i18next';
 
 interface ThumbType {
   data: any;
 }
-const Thumb: React.FC<ThumbType> = ({ data,t }) => {
+
+const Thumb: React.FC<ThumbType> = ({ data }) => {
+  const { t } = useTranslation();
   return (
     <section className="px-3 pt-3">
       <div className="row">
         {toJS(data).map((item: any) => {
           let status = '';
-          if (item.status == 1) {
-            status = t('Running');
-          } else if (item.status == 2) {
-            status = t('Schedule');
+          if (item.status === 1) {
+            status = t('txt_running');
+          } else if (item.status === 2) {
+            status = t('txt_schedule');
           } else {
-            status = t('Stop');
+            status = t('txt_stop');
           }
-          const classStatus = status.toLowerCase().replace(/\s/g, '');
+
           return (
             <div className="col-md-3 mb-4 " key={item.id}>
-              <div className="bg-white rounded-3 p-3 overflow-y-hidden mh-350px">
+              <div className="bg-white rounded-3 p-3 overflow-y-hidden h-100">
                 <p>{item.logo}</p>
                 <p>
                   <span
-                    className={`px-2 fs-14 rounded-1 d-inline-block project__status-${classStatus}`}
+                    className={`px-2 fs-14 rounded-1 d-inline-block ${
+                      status === t('txt_running')
+                        ? 'badge bg-posted mw-50 h-20 d-inline align-middle'
+                        : status === t('txt_schedule')
+                        ? 'badge bg-processing mw-50 h-20 d-inline align-middle'
+                        : 'badge bg-failed mw-50 h-20 d-inline align-middle'
+                    }`}
                   >
                     {status}
                   </span>
                 </p>
                 <h3 className=" text-name fs-5 fw-bold">{item.name.props.children}</h3>
-                <p className="text-description overflow-hidden h-100px">{item.shortDescription}</p>
+                <p className="text-description">{item.shortDescription}</p>
               </div>
             </div>
           );
@@ -42,4 +50,5 @@ const Thumb: React.FC<ThumbType> = ({ data,t }) => {
   );
 };
 
-export default withTranslation()(Thumb);
+const ThumbWithTranslation = withTranslation()(Thumb);
+export { ThumbWithTranslation as Thumb };
