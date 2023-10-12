ffc75fe41c858cf8b521b12bc66c971149731aa2
lamanhvu62 <103493256+lamanhvu62@users.noreply.github.com>
2023-10-12 11:13:31 +0700

    fix  var

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -119,7 +119,7 @@ const Table = ({
   useEffect(() => {
     const selectedIds = Object.keys(selectedRowIds);
     if (selectedIds.length > 0) {
-      var selectedRowsData = selectedIds
+      let selectedRowsData = selectedIds
         .map((x) => data[x])
         .filter(function (x) {
           return x != null;
