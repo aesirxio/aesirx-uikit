6b94c966dc087cdbc6cd4378af16f86b25a6b816
toanredweb <toan@r-digital.tech>
2023-11-02 06:41:43 +0700

    Fixes #62 License

---
+++ src/pages/Licenses/UpdateLicense.tsx
@@ -0,0 +1,128 @@
+import { useFormik } from 'formik';
+import React, { useState } from 'react';
+import { Alert, Button, Form, Modal, Spinner } from 'react-bootstrap';
+import * as Yup from 'yup';
+import { updateMember } from '../../store/UtilsStore/aesirx';
+import { useGlobalContext } from '../../providers/global';
+import { notify } from '../../components/Toast';
+interface Props {
+  data: {
+    name: string;
+    data: {
+      domain: string;
+      test_domain: string;
+    };
+  };
+  show: boolean;
+  setShow: any;
+}
+
+const UpdateLicense = (props: Props) => {
+  const [loading, setLoading] = useState(false);
+  const { accessToken } = useGlobalContext();
+
+  const formik = useFormik({
+    initialValues: {
+      domain: props.data.data.domain ?? '',
+      test_domain: props.data.data.test_domain ?? '',
+    },
+    validationSchema: Yup.object({
+      domain: Yup.string()
+        .required('Please enter your domain')
+        .matches(/^((ftp:|http:|https:):\/\/)?(www.)?(?!.*(ftp:|http:|https:|www.))/gm, 'Invalid'),
+      test_domain: Yup.string()
+        .required('Please enter your test domain')
+        .matches(/^((ftp:|http:|https:):\/\/)?(www.)?(?!.*(ftp|http|https|www.))/gm, 'Invalid'),
+    }),
+    onSubmit: async (values) => {
+      setLoading(true);
+      try {
+        const response = await updateMember(
+          { ...values, license_key: props?.data?.name },
+          accessToken
+        );
+        if (response?.result?.success) {
+          props.data.data.domain = values.domain;
+          props.data.data.test_domain = values.test_domain;
+          notify('Update license successfully!', 'success');
+          props.setShow({ show: false });
+        }
+      } catch (error: any) {
+        console.log('UpdateMember error', error);
+        notify(error?.message , "error");
+      }
+
+      setLoading(false);
+    },
+  });
+
+  if (!props?.data) {
+    return null;
+  }
+
+  return (
+    <Modal show={props.show} onHide={() => props.setShow({ show: false })} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit License</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group className="mb-3" id="formGroupdomain">
+            <Form.Label className="mb-2">Domain</Form.Label>
+            <Form.Control
+              id="domain"
+              name="domain"
+              className={`rounded py-12px lh-sm mb-2 ${
+                formik.touched.domain && formik.errors.domain ? 'border-danger' : ''
+              }`}
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.domain}
+            />
+            {formik.touched.domain && formik.errors.domain ? (
+              <Alert className="mb-0 p-0 bg-white border-0 text-danger d-flex align-items-center">
+                <span className="fs-14 fw-semibold ms-2 lh-1">{formik.errors.domain}</span>
+              </Alert>
+            ) : null}
+          </Form.Group>
+          <Form.Group className="mb-3" id="formGrouptest_domain">
+            <Form.Label className="mb-2">Test Domain</Form.Label>
+            <Form.Control
+              id="test_domain"
+              name="test_domain"
+              className={`rounded py-12px lh-sm mb-2 ${
+                formik.touched.test_domain && formik.errors.test_domain ? 'border-danger' : ''
+              }`}
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.test_domain}
+            />
+            {formik.touched.test_domain && formik.errors.test_domain ? (
+              <Alert className="mb-0 p-0 bg-white border-0 text-danger d-flex align-items-center">
+                <span className="fs-14 fw-semibold ms-2 lh-1">{formik.errors.test_domain}</span>
+              </Alert>
+            ) : null}
+          </Form.Group>
+          <Button
+            type="submit"
+            disabled={!formik?.isValid || loading}
+            variant="success"
+            className=" lh-sm w-100 fw-semibold py-12px"
+          >
+            {loading ? <Spinner /> : 'Update License'}
+          </Button>
+          <Alert className="mt-1 p-1">
+            <span className="text-warning fs-7 fst-italic">
+              Tip: A URL with a non-standard port and without http/https and www:
+              Ex:partners.aesirx.io
+            </span>
+          </Alert>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default UpdateLicense;
---
+++ src/store/UtilsStore/concordium.ts
@@ -0,0 +1,235 @@
+import { TESTNET, MAINNET, Network } from '@concordium/react-components';
+import {
+  AccountAddress,
+  AccountTransactionType,
+  CcdAmount,
+  deserializeReceiveReturnValue,
+  SchemaVersion,
+  TransactionStatusEnum,
+} from '@concordium/web-sdk';
+
+import { Buffer } from 'buffer/';
+
+async function waitForFinalizedTransaction(transactionHash: any, connection: any) {
+  let transactionStatus = null;
+  let txnStatus = null;
+  const client = await connection.getJsonRpcClient();
+
+  while (transactionStatus !== TransactionStatusEnum.Finalized) {
+    txnStatus = await client.getTransactionStatus(transactionHash);
+    transactionStatus = txnStatus?.status;
+
+    console.info(`txn : ${transactionHash}, status: ${txnStatus?.status}`);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second
+  }
+
+  console.log('Transaction has been finalized', txnStatus);
+  return ensureValidOutcome(txnStatus?.outcomes);
+}
+
+const ensureValidOutcome = (outcomes: any) => {
+  if (!outcomes) {
+    throw Error('Null Outcome');
+  }
+
+  let successTxnSummary = Object.keys(outcomes)
+    .map((k) => outcomes[k])
+    .find((s) => s.result.outcome === 'success');
+
+  if (!successTxnSummary) {
+    let failures = Object.keys(outcomes)
+      .map((k) => outcomes[k])
+      .filter((s) => s.result.outcome === 'reject')
+      .map((s) => s.result.rejectReason.tag)
+      .join(',');
+    throw Error(`Transaction failed, reasons: ${failures}`);
+  }
+
+  return successTxnSummary;
+};
+
+const updateSmartContract = async (
+  connection: any,
+  account: any,
+  name: any,
+  amount: any,
+  index: any,
+  subIndex: any,
+  schema: any,
+  method: any,
+  params = {}
+) => {
+  try {
+    const txnHash = await connection.signAndSendTransaction(
+      account,
+      AccountTransactionType.Update,
+      {
+        amount: toCcd(BigInt(amount)),
+        address: {
+          index: BigInt(index),
+          subindex: BigInt(subIndex),
+        },
+        receiveName: `${name}.${method}`,
+        maxContractExecutionEnergy: BigInt(6000),
+      },
+      params,
+      schema
+    );
+
+    return await waitForFinalizedTransaction(txnHash, connection);
+  } catch (error: any) {
+    throw Error(error);
+  }
+};
+
+const invokeSmartContract = async (
+  connection: any,
+  account: any,
+  name: any,
+  index: any,
+  subIndex: any,
+  schema: any,
+  method: any
+) => {
+  try {
+    const client = await connection.getJsonRpcClient();
+
+    const rawReturnValue = await client.invokeContract({
+      invoker: new AccountAddress(account),
+      contract: { index: BigInt(index), subindex: BigInt(subIndex) },
+      method: `${name}.${method}`,
+    });
+
+    const returnValue = await deserializeReceiveReturnValue(
+      Buffer.from(rawReturnValue.returnValue, 'hex'),
+      Buffer.from(schema, 'base64'),
+      name,
+      method,
+      SchemaVersion.V2
+    );
+
+    console.log('invokeSmartContract', returnValue);
+
+    return returnValue;
+  } catch (error: any) {
+    console.log('invokeSmartContract error', error);
+    return null;
+  }
+};
+
+const checkPaid = async (account: any, connection: any, web3id: any) => {
+  const data = await invokeSmartContract(
+    connection,
+    account,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_NAME,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_INDEX,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_SUBINDEX,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_RAWSCHEMA,
+    'view'
+  );
+
+  console.log('checkPaid', web3id, data, data?.paid_web3ids.includes(web3id));
+
+  if (data) {
+    return data?.paid_web3ids.includes(web3id);
+  }
+};
+
+const pay = async (account: any, connection: any, web3id: any) => {
+  try {
+    const outcomes = await updateSmartContract(
+      connection,
+      account,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_NAME,
+      process.env.NEXT_PUBLIC_CONCORDIUM_NETWORK === 'testnet' ? 100 : 30,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_INDEX,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_SUBINDEX,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_RAWSCHEMA,
+      'pay',
+      {
+        web3id: web3id,
+      }
+    );
+
+    return outcomes;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+const mintWeb3IDNFT = async (account: any, connection: any, web3id: any) => {
+  try {
+    return await updateSmartContract(
+      connection,
+      account,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_NAME,
+      0,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_INDEX,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_SUBINDEX,
+      process.env.NEXT_PUBLIC_SMARTCONTRACT_WHITELIST_RAWSCHEMA,
+      'mint_web3id',
+      {
+        web3id: web3id,
+      }
+    );
+  } catch (error: any) {
+    throw Error(error);
+  }
+};
+
+const checkMintWeb3IDNFT = async (account: any, connection: any) => {
+  const data = await invokeSmartContract(
+    connection,
+    account,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_NFT_WEB3ID_NAME,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_NFT_WEB3ID_INDEX,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_NFT_WEB3ID_SUBINDEX,
+    process.env.NEXT_PUBLIC_SMARTCONTRACT_NFT_WEB3ID_RAWSCHEMA,
+    'view'
+  );
+  console.log(
+    'checkMintWeb3IDNFT',
+    account,
+    data,
+    data?.state?.some((arrVal: any) => account === arrVal[0]?.Account[0])
+  );
+
+  if (data) {
+    return data?.state?.some((arrVal: any) => account === arrVal[0]?.Account[0]);
+  }
+};
+
+const MICRO_CCD_IN_CCD = 1000000;
+
+const toCcd = (ccdAmount: any) => {
+  return new CcdAmount(ccdAmount * BigInt(MICRO_CCD_IN_CCD));
+};
+
+const transactionLink = (network: Network, txHash: string) => {
+  return `${network.ccdScanBaseUrl}/?dcount=1&dentity=transaction&dhash=${txHash}`;
+};
+
+const checkNetwork = (hash: string) => {
+  switch (process.env.NEXT_PUBLIC_CONCORDIUM_NETWORK) {
+    case 'testnet':
+      return hash === TESTNET.genesisHash;
+
+    default:
+      return hash === MAINNET.genesisHash;
+  }
+};
+
+export {
+  waitForFinalizedTransaction,
+  updateSmartContract,
+  invokeSmartContract,
+  checkPaid,
+  pay,
+  mintWeb3IDNFT,
+  checkMintWeb3IDNFT,
+  transactionLink,
+  checkNetwork,
+  toCcd,
+  ensureValidOutcome,
+};
