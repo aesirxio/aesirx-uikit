43e20400979faa5cde3df92b20fda0d6e4ec58e4
toanredweb <toan@r-digital.tech>
2023-11-07 11:42:20 +0700

    update

--- src/components/CreateMarkup.tsx
+++ src/components/CreateMarkup.tsx
@@ -1,24 +1,5 @@
 import React from 'react';
-interface ImageIcon {
-  width?: number;
-  height?: number;
-  src: string;
-  className?: string;
-}
-
-const ImageIcon = ({ width, height, src, className }: ImageIcon) => {
-  return (
-    <span
-      className={`icon-image d-inline-block align-text-bottom ${className}`}
-      style={{
-        width: `${width ?? 24}px`,
-        height: `${height ?? 24}px`,
-        WebkitMaskImage: `url(${src})`,
-        WebkitMaskRepeat: 'no-repeat',
-        WebkitMaskSize: 'contain',
-      }}
-    ></span>
-  );
+const CreateMarkup = ({ htmlString, className }: any) => {
+  return <span className={className ?? ''} dangerouslySetInnerHTML={{ __html: htmlString }}></span>;
 };
-
-export default ImageIcon;
+export default CreateMarkup;
